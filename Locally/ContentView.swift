96ae90207e8d63b6129bc70c1e61832299243c2b
Mihail87 <leontemihai@gmail.com>
2019-10-31 16:40:44 +0200

    Project setup

---
+++ Locally/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Locally
+//
+//  Created by Mihai Leonte on 10/31/19.
+//  Copyright © 2019 Mihai Leonte. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
